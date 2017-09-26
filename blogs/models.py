21925bbbc688b21ebd36eef30de70eba522ea7ea
waqar-ali141 <waqar.ali@arbisoft.com>
2017-09-26 15:31:15 +0500

    Updated models

--- blogs/models.py
+++ blogs/models.py
@@ -5,7 +5,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Categories(models.Model):
+class Category(models.Model):
     name = models.CharField(max_length=35, verbose_name='Category Name')
 
     @property
@@ -13,23 +13,24 @@ class Categories(models.Model):
         return self.relative_posts.all()
 
 
-class Posts(models.Model):
+class Post(models.Model):
     title = models.CharField(max_length=35)
     description = models.TextField()
 
-    category_belongs_to = models.ForeignKey(Categories, related_name='posts', on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, related_name='posts', on_delete=models.CASCADE)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='post_owner')
     date_created = models.DateField()
 
 
-class Comments(models.Model):
+class Comment(models.Model):
     comment = models.TextField()
-    post_belongs_to = models.ForeignKey(Posts, related_name='comments', on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
 
 
-class Likes(models.Model):
-    count = models.IntegerField()
-    comments_belongs_to = models.OneToOneField(Comments, on_delete=models.CASCADE, related_name='likes')
+class Like(models.Model):
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE, related_name='likes')
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
 
 
 
