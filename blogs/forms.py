eb7395a06c3138341721ffc4a55f814a1cf8847a
waqarali141 <waqar.ali141@gmail.com>
2017-09-27 10:25:08 +0500

    Updated Models

--- blogs/forms.py
+++ blogs/forms.py
@@ -2,10 +2,16 @@ __author__ = 'waqarali'
 
 from django.forms import ModelForm
 
-from blogs.models import Category
+from blogs.models import Category, Post
 
 
 class CategoryForm(ModelForm):
     class Meta:
         model = Category
         fields = ['name']
+
+
+class PostForm(ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'description']
--- blogs/urls.py
+++ blogs/urls.py
@@ -6,7 +6,16 @@ from . import views
 
 app_name = 'blog'
 urlpatterns = [
-    url(r'^$', views.CategoryIndexView.as_view(), name='CategoryIndex'),
-    url(r'^login$', auth_views.login, name='login'),
-    url(r'^post$', views.CreateCategoryView.as_view(), name='CategoryPost')
+    url(r'^$', views.CategoryIndexView.as_view(),
+        name='CategoryIndex'),
+    url(r'^login$', auth_views.login,
+        name='login'),
+    url(r'^post$', views.CreateCategoryView.as_view(),
+        name='CategoryPost'),
+    url(r'^post/delete/(?P<pk>[0-9 a-z A-Z]+)$', views.DeleteCategoryView.as_view(),
+        name='DeleteCategory'),
+    url(r'^post/(?P<pk>[0-9 a-z A-Z]+)$', views.PostIndexView.as_view(),
+        name='PostIndex'),
+    url(r'post/(?P<pk>[0-9 a-z A-Z]+)/add$', views.CreatePostView.as_view(),
+        name='CreatePost')
 ]
