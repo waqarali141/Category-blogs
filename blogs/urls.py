ff4c8d6f220bfa640da757f4061bc2e744f49573
waqar-ali141 <waqar.ali@arbisoft.com>
2017-09-28 18:23:54 +0500

    Urls Scheme Updated

--- blogs/urls.py
+++ blogs/urls.py
@@ -15,43 +15,44 @@ urlpatterns = [
     url(r'^logout$', auth_views.logout,
         name='logout'),
 
-    url(r'^category/add$', views.CreateCategoryView.as_view(),
+    url(r'^category/create$', views.CreateCategoryView.as_view(),
         name='CategoryPost'),
 
-    url(r'^category/delete/(?P<pk>[0-9 a-z A-Z]+)$', views.DeleteCategoryView.as_view(),
+    url(r'^category/(?P<pk>[0-9 a-z A-Z]+)/delete$', views.DeleteCategoryView.as_view(),
         name='DeleteCategory'),
 
     url(r'^category/(?P<pk>[0-9 a-z A-Z]+)$', views.CategoryDetailView.as_view(),
         name='PostIndex'),
 
-    url(r'category/(?P<pk>[0-9 a-z A-Z]+)/post/add$', views.CreatePostView.as_view(),
+    url(r'category/(?P<pk>[0-9 a-z A-Z]+)/post/create$', views.CreatePostView.as_view(),
         name='CreatePost'),
 
-    url(r'category/(?P<Cid>[0-9 a-z A-Z]+)/post/delete/(?P<pk>[0-9 a-z A-Z]+)>', views.DeletePostView.as_view(),
+    url(r'category/(?P<Cid>[0-9 a-z A-Z]+)/post/(?P<pk>[0-9 a-z A-Z]+)>/delete$', views.DeletePostView.as_view(),
         name='DeletePost'),
 
     url(r'category/(?P<Cid>[0-9 a-z A-Z]+)/post/(?P<pk>[0-9 a-z A-Z]+)$', views.PostDetailView.as_view(),
         name='PostDetail'),
 
-    url(r'category/(?P<Cid>[0-9 a-z A-Z]+)/post/(?P<pk>[0-9 a-z A-Z]+)/add/comment$', views.CreateCommentView.as_view(),
+    url(r'category/(?P<Cid>[0-9 a-z A-Z]+)/post/(?P<pk>[0-9 a-z A-Z]+)/comment/create$',
+        views.CreateCommentView.as_view(),
         name='CreateComment'),
 
-    url(r'category/(?P<Cid>[0-9 a-z A-Z]+)/post/(?P<Pid>[0-9 a-z A-Z]+)/delete/comment/(?P<pk>[0-9 a-z A-Z]+)$',
+    url(r'category/(?P<Cid>[0-9 a-z A-Z]+)/post/(?P<Pid>[0-9 a-z A-Z]+)/comment/(?P<pk>[0-9 a-z A-Z]+)/delete$',
         views.DeleteCommentView.as_view(),
         name='DeleteComment'),
 
     url(r'category/(?P<Cid>[0-9 a-z A-Z]+)/post/(?P<pk>[0-9 a-z A-Z]+)/update$', views.PostUpdateView.as_view(),
         name='UpdatePostView'),
 
-    url(r'category/(?P<Cid>[0-9 a-z A-Z]+)/post/(?P<Pid>[0-9 a-z A-Z]+)/update/comment/(?P<pk>[0-9 a-z A-Z]+)$',
+    url(r'category/(?P<Cid>[0-9 a-z A-Z]+)/post/(?P<Pid>[0-9 a-z A-Z]+)/comment/(?P<pk>[0-9 a-z A-Z]+)/update$',
         views.CommentUpdateView.as_view(),
         name='CommentUpdateView'),
 
-    url(r'category/(?P<CatId>[0-9 a-z A-Z]+)/post/(?P<Pid>[0-9 a-z A-Z]+)/like/comment/(?P<CommentId>[0-9 a-z A-Z]+)',
+    url(r'category/(?P<CatId>[0-9 a-z A-Z]+)/post/(?P<Pid>[0-9 a-z A-Z]+)/comment/(?P<CommentId>[0-9 a-z A-Z]+)/like',
         views.commentlikeview,
         name='CommentLike'),
 
-    url(r'category/(?P<CatId>[0-9 a-z A-Z]+)/post/(?P<Pid>[0-9 a-z A-Z]+)/unlike/comment/(?P<CommentId>[0-9 a-z A-Z]+)',
+    url(r'category/(?P<CatId>[0-9 a-z A-Z]+)/post/(?P<Pid>[0-9 a-z A-Z]+)/comment/(?P<CommentId>[0-9 a-z A-Z]+)/unlike',
         views.commentunlikeview,
         name='CommentUnLike'),
 
